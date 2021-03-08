9dd9dca92e579037fb555c1929a8ae2e9d10dd10
EriKaffeKanN <errik.jag@gmail.com>
2021-03-08 16:58:26 +0100

    Nya sidor html
    + andra fixar / styling

---
+++ js/scrollingeffects.js
@@ -0,0 +1,22 @@
+const getIndexFromTag = (emtList, tagName) => {
+    index = undefined;
+    for(let e = 0; e < emtList.length; e++) {
+        if(emtList[e].tagName == tagName.toUpperCase()) {
+            index = e;
+        }
+    }
+    return index;
+}
+
+/*
+const section1 = document.getElementsByTagName("section")[0];
+const textBox1 = section1.children[getIndexFromTag(section1.children, "article")];
+
+setInterval(() => {
+    textBox1.style.marginLeft = easingFunction(window.scrollY, 10, 0.01) + "em";
+}, 1);
+
+const easingFunction = (x, limit, easing) => {
+    return limit - (1/Math.exp(easing*x+Math.log(1/limit)));
+}
+*/
